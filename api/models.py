4f589180303533a595db3effc9f2b88a86426619
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-09 20:19:45 +0500

    update: update models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Room(models.Model):
+    room_number = models.IntegerField(unique=True)
+    is_booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Room {self.room_number}"
