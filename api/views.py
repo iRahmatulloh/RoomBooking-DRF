cce0f4bbbac3858c0707d6ad2a477729db370f2d
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-10 17:40:24 +0500

    Add: Added google views

--- api/views.py
+++ api/views.py
@@ -36,6 +36,33 @@ class FacebookRoomView(APIView):
             return Response({"message": "Xona topilmadi yoki mavjud emas xona raqami kiritildi!"}, status=status.HTTP_404_NOT_FOUND)
 
 
+class GoogleRoomView(APIView):
+    def get(self, request, room_number):
+        try:
+            room = GoogleRoom.objects.get(room_number=room_number)
+            #
+            if room.is_booked:
+                serializer = RoomIsBookedSerializer(room)
+
+                msg = "Xona allaqachon boshqa bir mijoz tomonidan band qilingan!"
+                data = {
+                    "message": msg,
+                    "available_from": serializer.data.values()
+                }
+                return Response(data, status=status.HTTP_409_CONFLICT)
+            else:
+                serializer = RoomIsNotBookedSerializer(room)
+                print(serializer.data)
+                msg = "Xona band qilinmagan!"
+                data = {
+                    "message": msg,
+                    "room": serializer.data
+                }
+                return Response(data, status=status.HTTP_201_CREATED)
+
+        except GoogleRoom.DoesNotExist:
+            return Response({"message": "Xona topilmadi yoki mavjud emas xona raqami kiritildi!"}, status=status.HTTP_404_NOT_FOUND)
+
 
 
 
