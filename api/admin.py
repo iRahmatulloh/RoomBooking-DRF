5cd4de6c693bc12894455aaf3eaf7feb3377db42
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-10 18:34:26 +0500

    update admin file

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import FacebookRoom
+from .models import *
 # Register your models here.
 admin.site.register(FacebookRoom)
+admin.site.register(AmazonRoom)
+admin.site.register(GoogleRoom)
+admin.site.register(NetflixRoom)
+admin.site.register(HuluRoom)
