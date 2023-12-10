4624c9943d0c9f22a3c4f33154cd13a7bb7b43e3
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-10 16:21:59 +0500

    Update: updated model, added start and end time

--- api/models.py
+++ api/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
-class Room(models.Model):
+class FacebookRoom(models.Model):
     room_number = models.IntegerField(unique=True)
     is_booked = models.BooleanField(default=False)
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"Room {self.room_number}"
