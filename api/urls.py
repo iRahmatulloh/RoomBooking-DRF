990b0f1eaf8760d987dac2a9398a5902f44c6258
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-10 16:25:19 +0500

    Add: added facebookRoom url, line 7

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import hello_world
+from .views import hello_world, FacebookRoomView
 
 urlpatterns = [
     path('api/', hello_world, name='home'),
+    path('api/facebook/<int:room_number>/', FacebookRoomView.as_view(), name='FaceBook-Room'),
 ]
