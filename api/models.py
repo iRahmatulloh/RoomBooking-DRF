7f147dbde760de7c71c76a1bb2408d89233ac00a
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-10 17:30:03 +0500

    Add: Added HuluRoom

--- api/models.py
+++ api/models.py
@@ -41,3 +41,13 @@ class NetflixRoom(models.Model):
 
     def __str__(self):
         return f"Room {self.room_number}"
+
+
+class HuluRoom(models.Model):
+    room_number = models.IntegerField(unique=True)
+    is_booked = models.BooleanField(default=False)
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Room {self.room_number}"
