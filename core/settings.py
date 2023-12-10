f1bee73d9acf6de6754a337691e2e901bcee74bd
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-10 21:50:21 +0500

    added: added psycopg2

--- core/settings.py
+++ core/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'core.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': "default",
+        'HOST': "ep-wispy-mountain-51143717-pooler.us-east-1.postgres.vercel-storage.com",
+        'PASSWORD': "pI9beRkSYW8t"
+
     }
 }
 
