d59e3eb97c049869fc542da5e46ab40af5bba877
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-10 17:28:24 +0500

    Add: Added AmazonRoom

--- api/models.py
+++ api/models.py
@@ -21,3 +21,13 @@ class GoogleRoom(models.Model):
 
     def __str__(self):
         return f"Room {self.room_number}"
+
+
+class AmazonRoom(models.Model):
+    room_number = models.IntegerField(unique=True)
+    is_booked = models.BooleanField(default=False)
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Room {self.room_number}"
