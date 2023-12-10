41bf14e75dab0ca0685ce6172c1d5dfa32751a97
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-10 17:29:10 +0500

    Add: Added NetflixRoom

--- api/models.py
+++ api/models.py
@@ -31,3 +31,13 @@ class AmazonRoom(models.Model):
 
     def __str__(self):
         return f"Room {self.room_number}"
+
+
+class NetflixRoom(models.Model):
+    room_number = models.IntegerField(unique=True)
+    is_booked = models.BooleanField(default=False)
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Room {self.room_number}"
